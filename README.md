17d3be792ce007082bf8f3cc3c3daccda6527722
Chris Lee <christopher.lee@concur.com>
2018-07-25 13:41:50 -0700

    intitial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple HTTP test server
+
+Returns environment variables as a JSON. 
+
+Setting 'DOWNSTREAM' env to a URL will attempt a GET request and print the results
---
+++ main.go
@@ -0,0 +1,59 @@
+// This is the name of our package
+// Everything with this package name can see everything
+// else inside the same package, regardless of the file they are in
+package main
+
+// These are the libraries we are going to use
+// Both "fmt" and "net" are part of the Go standard library
+import (
+	// "fmt" has methods for formatted I/O operations (like printing to the console)
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+	// The "net/http" library has methods to implement HTTP clients and servers
+	"net/http"
+)
+
+func main() {
+	// The "HandleFunc" method accepts a path and a function as arguments
+	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
+	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
+	http.HandleFunc("/", handler)
+
+	// After defining our server, we finally "listen and serve" on port 8080
+	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
+	// and the handler defined above (in "HandleFunc") is used
+	http.ListenAndServe(":8080", nil)
+}
+
+// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
+// as the arguments.
+func handler(w http.ResponseWriter, r *http.Request) {
+	// For this case, we will always pipe "Hello World" into the response writer
+	json := "{"
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		json = json + "\"" + pair[0] + "\":" + "\"" + pair[1] + "\","
+	}
+	json = strings.TrimRight(json, ",") + "}"
+	fmt.Fprintf(w, json)
+	if os.Getenv("DOWNSTREAM") != "" {
+		fmt.Fprintf(w, getDownstream(os.Getenv("DOWNSTREAM")))
+	}
+}
+
+func getDownstream(url string) string {
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	response, err := netClient.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	return string(body)
+}
